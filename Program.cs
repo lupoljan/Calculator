fe93a21fd75293f9d35dcad34d4581137ad2b0a6
lupoljan <lupoljan@o2.pl>
2025-04-04 12:17:50 +0200

    calculte sum of 2 numbers

--- Program.cs
+++ Program.cs
@@ -6,15 +6,29 @@ using System.Threading.Tasks;
 
 namespace Calculator
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
             Console.Write("Enter your name: ");
             string name = Console.ReadLine();
             Console.WriteLine($"Hello, {name}!");
+
+            Console.Write("Enter first number: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int sum = CalculateSum(num1, num2);
+            Console.WriteLine($"The sum of {num1} and {num2} is {sum}.");
+
             Console.ReadKey();
+        }
+
+        public static int CalculateSum(int a, int b)
+        {
+            return a + b;
         }
     }
 }
