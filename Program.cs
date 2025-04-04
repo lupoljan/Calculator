a416f14dc8a8b8a3f55e07c9e091140d3d44ff71
lupoljan <lupoljan@o2.pl>
2025-04-04 12:26:30 +0200

    diffrerence of 2 numbers

--- Program.cs
+++ Program.cs
@@ -6,15 +6,29 @@ using System.Threading.Tasks;
 
 namespace Calculator
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
             Console.Write("Enter your name: ");
             string name = Console.ReadLine();
             Console.WriteLine($"Hello, {name}!");
+
+
+            Console.Write("Enter first number: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            int b = int.Parse(Console.ReadLine());
+
+            int sum = CalculateDifference(a, b);
+            Console.WriteLine($"{a} - {b} = {sum}");
             Console.ReadKey();
+        }
+
+        public static int CalculateDifference(int a, int b)
+        {
+            return a - b;
         }
     }
 }
